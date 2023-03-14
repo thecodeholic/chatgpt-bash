8cc0d21bdc6b919def01615983bf2356ecb6eae6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-03-14 14:21:05 +0400

    Handle API response error and render errors in red color

--- chatgpt.sh
+++ chatgpt.sh
@@ -3,12 +3,18 @@
 url="https://api.openai.com/v1/chat/completions"
 apikey="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"
 
+# Define red color code
+COLOR_RED='\033[0;31m'
+
+# Define reset color code
+COLOR_NC='\033[0m'
+
 install_jq() {
     # Check if jq command is available
     if command -v jq > /dev/null; then
         return 0
     else
-        echo "\"jq\" is required to run this script. Exiting..."
+        echo -e "${COLOR_RED}\"jq\" is required to run this script. Exiting...${COLOR_NC}"
         exit 1
     fi
 }
@@ -34,6 +40,14 @@ while true; do
 
     # Make request to OpenAI API
     response=$(curl $url -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $apikey" -d "$data" -s )
+
+    error=$(echo "$response" | jq -r '.error.message')
+    echo $response
+    if [ "$error" != "null" ]; then
+        echo -e "${COLOR_RED}!!! ERROR !!!"
+        echo -e "$error${COLOR_NC}"
+        continue
+    fi
 
     # Get Response, extract content and print
     answer=$(echo "$response" | jq -r '.choices[0].message.content')
