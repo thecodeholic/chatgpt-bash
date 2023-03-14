7f69d5c1902a618678cc7006a5a7c1323aaec4be
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-03-14 14:14:35 +0400

    Create while loop to read user input

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Hello! I am ChatGPT in your linux terminal. Ask me anything..."
+
 while true; do
+
+    # Read the user input, also enable arrow navigation
     read -e -p "> " input
 
+    # If the input is empty continue
+    if ! [ -n "$input" ]; then
+      continue
+    fi
+    
     echo $input
 done
