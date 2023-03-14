171f9231aeb92a899bd30016b46930cf1de780e7
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-03-14 14:27:51 +0400

    Ask the user to enter API key and save it for later usage

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+file_path=~/.openapi.json
 url="https://api.openai.com/v1/chat/completions"
-apikey="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"
+apikey=
 
 # Define red color code
 COLOR_RED='\033[0;31m'
@@ -19,7 +20,33 @@ install_jq() {
     fi
 }
 
+read_api_key() {
+    while true; do
+        # If file exists that should contain apikey
+        if test -f $file_path; then
+            # We read that file and retun from the function
+            apikey=$(cat $file_path | jq -r '.apikey')
+
+            return 0
+        else
+            # If file does not exist we ask user to enter his api key
+            read -p "Please enter your OpenAI API Key > " apikey
+
+            # If user entered non empty apikey we save that in $file_path location
+            if [ -n "$apikey" ]; then
+                echo "Your API key was stored in \"$file_path\" for later usage"
+                echo "{\"apikey\": \"$apikey\"}" > $file_path
+                return 0
+            fi
+        fi
+
+    done
+
+}
+
 install_jq
+
+read_api_key
 
 echo "Hello! I am ChatGPT in your linux terminal. Ask me anything..."
 
@@ -42,7 +69,7 @@ while true; do
     response=$(curl $url -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $apikey" -d "$data" -s )
 
     error=$(echo "$response" | jq -r '.error.message')
-    echo $response
+    
     if [ "$error" != "null" ]; then
         echo -e "${COLOR_RED}!!! ERROR !!!"
         echo -e "$error${COLOR_NC}"
