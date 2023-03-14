2dce758fd65d6afe519129a479a95aae9f46e484
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-03-14 14:17:32 +0400

    Make request to openapi api and get response

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+url="https://api.openai.com/v1/chat/completions"
+apikey="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"
+
 echo "Hello! I am ChatGPT in your linux terminal. Ask me anything..."
 
 while true; do
-
     # Read the user input, also enable arrow navigation
     read -e -p "> " input
 
@@ -11,6 +13,15 @@ while true; do
     if ! [ -n "$input" ]; then
       continue
     fi
-    
-    echo $input
+
+    # Prepare message(s)
+    messages="{\"role\": \"user\", \"content\": \"$input\"}"
+
+    # Prepare request payload data
+    data="{\"model\": \"gpt-3.5-turbo\",\"messages\": [$messages]}"
+
+    # Make request to OpenAI API
+    response=$(curl $url -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $apikey" -d "$data" -s )
+
+    echo $response
 done
